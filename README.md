1094b348625f07f50e25ec3e18768ab56eeeb35d
Hongbo Jing <hongboing@Hongbo-Jings-MacBook-Pro.local>
2016-05-03 00:59:01 -0400

    check out the markdown 2

--- README.md
+++ README.md
@@ -8,12 +8,14 @@ Go to this [link](http://shields.io/) to add **badge**
 
 Go to this [link](http://www.emoji-cheat-sheet.com/) to add **EMOJI**
 ## Installation
-1. *npm install*
+1. **npm install**
     * To install all the dependencies in the package.json file
-    * Test server installation: *nodemon server.js*
-2. *npm install -g bower*
+    * Test server installation: **nodemon server.js**
+2. **npm install -g bower**
     * Install bower : the package management library
-3.
+3. **bower install**
+    * run this command to install all the libraries that you declare in the *bower.json* file.
+    * I use this simple command because I already indicate in the *bower.json* file that I need *angularJS* *angularJS route* *angularJS animate* *bootstrap* and *jquery* in this project, if you want to install other libraraies you want, you can run this: **bower install <package name> --save**, we already have a *.bowerrc* file to set the directory for the project, where we will install the libraries.
 4.
 5.
 6.
--- public/assets/libs/angular-animate/angular-animate.js
+++ public/assets/libs/angular-animate/angular-animate.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.8
+ * @license AngularJS v1.3.20
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -438,9 +438,11 @@ angular.module('ngAnimate', ['ng'])
         //so that all the animated elements within the animation frame
         //will be properly updated and drawn on screen. This is
         //required to perform multi-class CSS based animations with
-        //Firefox. DO NOT REMOVE THIS LINE.
-        var a = bod.offsetWidth + 1;
-        fn();
+        //Firefox. DO NOT REMOVE THIS LINE. DO NOT OPTIMIZE THIS LINE.
+        //THE MINIFIER WILL REMOVE IT OTHERWISE WHICH WILL RESULT IN AN
+        //UNPREDICTABLE BUG THAT IS VERY HARD TO TRACK DOWN AND WILL
+        //TAKE YEARS AWAY FROM YOUR LIFE!
+        fn(bod.offsetWidth);
       });
     };
   }])
@@ -1198,17 +1200,20 @@ angular.module('ngAnimate', ['ng'])
           }
 
           return cache.promise = runAnimationPostDigest(function(done) {
-            var parentElement = element.parent();
-            var elementNode = extractElementNode(element);
-            var parentNode = elementNode.parentNode;
+            var cache, parentNode, parentElement, elementNode = extractElementNode(element);
+            if (elementNode) {
+              cache = element.data(STORAGE_KEY);
+              element.removeData(STORAGE_KEY);
+
+              parentElement = element.parent();
+              parentNode = elementNode.parentNode;
+            }
+
             // TODO(matsko): move this code into the animationsDisabled() function once #8092 is fixed
             if (!parentNode || parentNode['$$NG_REMOVED'] || elementNode['$$NG_REMOVED']) {
               done();
               return;
             }
-
-            var cache = element.data(STORAGE_KEY);
-            element.removeData(STORAGE_KEY);
 
             var state = element.data(NG_ANIMATE_STATE) || {};
             var classes = resolveElementClasses(element, cache, state.active);
@@ -1333,8 +1338,7 @@ angular.module('ngAnimate', ['ng'])
           } else if (lastAnimation.event == 'setClass') {
             animationsToCancel.push(lastAnimation);
             cleanup(element, className);
-          }
-          else if (runningAnimations[className]) {
+          } else if (runningAnimations[className]) {
             var current = runningAnimations[className];
             if (current.event == animationEvent) {
               skipAnimation = true;
@@ -1875,7 +1879,7 @@ angular.module('ngAnimate', ['ng'])
           return;
         }
 
-        if (!staggerTime && styles) {
+        if (!staggerTime && styles && Object.keys(styles).length > 0) {
           if (!timings.transitionDuration) {
             element.css('transition', timings.animationDuration + 's linear all');
             appliedStyles.push('transition');
